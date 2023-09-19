2f5e92f3c7a8a57d83bd300363d19af6f7fda1b2
IvanStanoev <145491986+IvanStanoev@users.noreply.github.com>
2023-09-20 01:59:12 +0300

    update
    hardExersice

---
+++ JavaScript-Coding/fundamentalModules/exerciseBasic/10.piramid.js
@@ -0,0 +1,16 @@
+function piramid(base,increment) {
+
+    let currentFloor = 0;
+    let stone = 0;
+    let marble = 0;
+    let lapis = 0;
+    let gold = 0;
+    let sumStone = 0;
+    let sumMarble = 0;
+
+    for (let floor = 1; floor <= base; floor++) {
+
+        currentFloor = floor;
+    }
+}
+piramid(11,1);
