002689d4bc5c0513c1f4c3aad04f699d1f1e0743
IvanStanoev <145491986+IvanStanoev@users.noreply.github.com>
2023-09-20 13:22:52 +0300

    hardExersize
    Too much time.

--- JavaScript-Coding/fundamentalModules/exerciseBasic/10.piramid.js
+++ JavaScript-Coding/fundamentalModules/exerciseBasic/10.piramid.js
@@ -1,8 +1,51 @@
-function piramid(base,increment) {
+function piramid(base, increment) {
 
-    while (base >= 1) {
-        console.log(base);
-        base --;
+    let stones = 0;
+    let marbles = 0;
+    let lapis = 0;
+    let gold = 0;
+    let countFloors = 0;
+    let currentSumOfBlocks = 0;
+
+    while (base >= 0) {
+
+        countFloors += 1;
+        currentSumOfBlocks = ((base * base) * increment);
+        let currentStones = 0;
+
+        if (countFloors % 10 == 5 || countFloors % 10 == 0) {
+
+            leftBlocks = ((base * base) * increment)
+            innerBlocks = (base - 2) * (base - 2);
+            lapis = leftBlocks - innerBlocks;
+            stones += innerBlocks;
+        }
+
+        if (base == increment) {
+            gold = ((base * base) * increment);
+            console.log(`Stone required: ${stones}`);
+            console.log(`Marble required: ${marbles}`);
+            console.log(`Lapis Lazuli required: ${lapis}`);
+            console.log(`Gold required: ${gold}`);
+            console.log(`Final pyramid height: ${countFloors}`);
+            return;
+        } else if (base < 0) {
+            console.log(`Stone required: ${stones}`);
+            console.log(`Marble required: ${marbles}`);
+            console.log(`Lapis Lazuli required: ${lapis}`);
+            console.log(`Gold required: ${gold}`);
+            console.log(`Final pyramid height: ${countFloors}`);
+            return;
+        }
+
+        if (countFloors != 5) {
+            currentStones = currentSumOfBlocks;
+            perimeter = ((base - 2) * (base - 2) * increment);
+            stones += perimeter;
+            marbles += ((base * base) * increment) - perimeter;
+        }
+        base -= 2;
+        base = base.Max_SAFE_INTEGER;
     }
 }
-piramid(11,1);
+piramid(11, 0.75);
