cb16302f630332e60b62ae6161a7d3190bf0c415
IvanStanoev <145491986+IvanStanoev@users.noreply.github.com>
2023-09-21 23:09:18 +0300

    save
    save

--- JavaScript-Coding/fundamentalModules/exerciseBasic/100.piramid.js
+++ JavaScript-Coding/fundamentalModules/exerciseBasic/100.piramid.js
@@ -4,14 +4,8 @@ function piramid(base, increment) {
     let marbles = 0;
     let lapis = 0;
     let gold = 0;
-    let countFloors = 0;
-    let currentSumOfBlocks = 0;
 
-    while (base >= 0) {
+    
 
-
-
-    base -= 2;
-    }
 }
-piramid(11, 0.75);
+piramid(11, 1);
---
+++ JavaScript-Coding/fundamentalModules/exerciseBasic/11.bitcoin.js
@@ -0,0 +1,6 @@
+let a = 9;
+if (a % 2 == 0) {
+    console.log(a);
+}else{
+    console.log(a);
+}
