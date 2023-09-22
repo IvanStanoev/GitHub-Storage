c93432f37313805d5e62b8ad5bd75665f476947d
IvanStanoev <145491986+IvanStanoev@users.noreply.github.com>
2023-09-22 14:02:37 +0300

    save
    saves

--- JavaScript-Coding/fundamentalModules/exerciseBasic/100.piramid.js
+++ JavaScript-Coding/fundamentalModules/exerciseBasic/100.piramid.js
@@ -1,11 +1,42 @@
 function piramid(base, increment) {
 
-    let stones = 0;
-    let marbles = 0;
+    let stone = 0;
+    let marble = 0;
     let lapis = 0;
     let gold = 0;
+    let countFloors = 0;
 
+    //inner layer should be from stones
+    //outer layer should be from marble
+    //every 5-th outer layer is made from lapis not marble
+    //final step is made from gold
+
+    while (base >= increment) {
+        
+        countFloors += 1;
+
+        //first step is base * base
+        currenSumOfBlocks = base * base;
+        
+
+        //every second floor - base is reduced by 2 (one for each side)
+        base -= 2;
+    }
     
+    
+
+    //stones - reduce base(current floor) with 2 and multiply with increment.
+    //lapis - for 5-th floor (lapis insteat marble)
+    //marble (base - 4) * inctement
+
+    //top layer = gold = base * increment
+
+    //print total amound (rounded up)
+    //final height (rounded dowm)
 
 }
 piramid(11, 1);
+
+
+
+   //Чети добре условията
--- JavaScript-Coding/fundamentalModules/exerciseBasic/11.bitcoin.js
+++
@@ -1,6 +0,0 @@
-let a = 9;
-if (a % 2 == 0) {
-    console.log(a);
-}else{
-    console.log(a);
-}
