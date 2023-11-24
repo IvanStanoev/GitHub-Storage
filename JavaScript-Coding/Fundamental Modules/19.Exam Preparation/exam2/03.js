b731ba0ebb1dd6eb5aa07d572a9f41b1b3bf66f0
IvanStanoev <145491986+IvanStanoev@users.noreply.github.com>
2023-11-24 20:45:56 +0200

    othe examples
    save

--- JavaScript-Coding/Fundamental Modules/19.Exam Preparation/exam2/03.js
+++ JavaScript-Coding/Fundamental Modules/19.Exam Preparation/exam2/03.js
@@ -1,11 +1,21 @@
 function numbers(str) {
-    
-    let nums = str.split(' ').map(Number);                                 // сплитваме по 'space'
-    
+
+    let nums = str.split(' ');
+    let num2 = [];
+    num2 = Number(nums);
+
     let avg = nums.reduce((acc, val) => acc + val) / nums.length;
 
-    console.log(avg);
+    let topNums = nums
+        .filter(num => num > avg)
+        .sort((a, b) => b - a)
+        .slice(0, 5);
 
+    if (topNums.length == 0) {
+        console.log('No');
+    } else {
+        console.log(topNums.join(' '));
+    }
 
 }
 numbers("10, 20, 30, 40, 50");
--- JavaScript-Coding/Fundamental Modules/solveSplitMethod.js
+++ JavaScript-Coding/Fundamental Modules/solveSplitMethod.js
@@ -3,8 +3,10 @@ let str; // Assume str is undefined
 
 // Check if str is defined before using split
 if (str !== undefined) {
+
     let parts = str.split(' '); // Split the string by space
     console.log(parts);
+    
 } else {
     console.log("The variable is undefined.");
 }
